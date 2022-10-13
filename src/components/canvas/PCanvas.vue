014063485f13efc098228d114c580b9462def687
Ethan Chaplin <ethan.p.chaplin@gmail.com>
2022-10-12 21:28:19 -0400

    Update git-clone dependency

---
+++ src/components/canvas/PCanvas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="canvas"></div>
+</template>
+
+<script>
+export default {
+  name: "PCanvas",
+};
+</script>
+
+<style>
+.canvas {
+  display: grid;
+  height: 100%;
+  width: 100%;
+}
+
+.display {
+  width: 100%;
+  height: 100%;
+  border-radius: 25px;
+  background-color: white;
+}
+</style>
